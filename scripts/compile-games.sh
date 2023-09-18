467a8db069adc7a8b693d5f8ba93a8cc283eee13
David Skoland <davidskoland@gmail.com>
2023-09-18 21:29:26 +0200

    Update pong and compile-games

--- scripts/compile-games.sh
+++ scripts/compile-games.sh
@@ -9,7 +9,17 @@ cd "${SCRIPT_DIR}/.."
 # Make sure the submodules are updated
 git submodule update --init
 
+# Stash eventual git changes
+git stash
+
 # Compile Go_Pong
 ./Go_Pong/scripts/build-wasm.sh
 
+# Copy and add to git
 cp ./Go_Pong/build/go_pong.wasm ./web/games/gopong/go_pong.wasm
+git add web/games/gopong/go_pong.wasm
+
+git commit -m "Update Go_Pong"
+
+# Pop git changes
+git stash pop
