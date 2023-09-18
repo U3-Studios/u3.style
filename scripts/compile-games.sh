7e2101bb6622494318c89db865ebaeaca41ad29e
David Skoland <davidskoland@gmail.com>
2023-09-18 21:47:55 +0200

    stop messing with git

--- scripts/compile-games.sh
+++ scripts/compile-games.sh
@@ -18,8 +18,3 @@ ${GOPONG_DIR}/scripts/build-wasm.sh
 
 # Copy to our folder
 cp ${GOPONG_DIR}/build/go_pong.wasm ./docs/games/gopong/go_pong.wasm
-
-# Git magic, add, commit, restore stashed changes
-git add docs/games/gopong/go_pong.wasm
-git commit -m "Update Go_Pong"
-git stash pop
